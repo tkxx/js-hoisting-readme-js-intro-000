3c0071fb870b40525989e8c23e41ac93318e6bbf
Tabitha Sin <tabitha.sin@gmail.com>
2020-01-28 05:07:26 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,31 @@
 function callMe() {
-  console.log("I just met you...");
-  console.log("and this is crazy..");
-  console.log("but here's my number..");
-  console.log("so call me");
-  return lyric;
   var lyric = "maybe";
+  // console.log("I just met you...");
+  // console.log("and this is crazy..");
+  // console.log("but here's my number..");
+  // console.log("so call me");
+  return lyric;
+
 }
 
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
 
-  var thisIsCrazy = function (){
+
+  function thisIsCrazy(){
     console.log("hey!!!")
   }
+  thisIsCrazy();
 }
 
 function sayMyName() {
   // fix the code in here:
-  var name = "Cricky";
-
-  sayMy();
+  var name;
 
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
   }
+  sayMy();
 }
