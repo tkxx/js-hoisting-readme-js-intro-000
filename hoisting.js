48e5e28ebafc0c93f93c7c369e4fc3d31c3bc5f2
Tabitha Sin <tabitha.sin@gmail.com>
2020-01-28 05:03:25 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,33 +1,33 @@
 function callMe() {
-  console.log("I just met you...");
-  console.log("and this is crazy..");
-  console.log("but here's my number..");
-  console.log("so call me");
   var lyric = "maybe";
+  // console.log("I just met you...");
+  // console.log("and this is crazy..");
+  // console.log("but here's my number..");
+  // console.log("so call me");
   return lyric;
+
 }
 
 
 function crazy() {
   // fix the code in here:
-  // thisIsCrazy();
+
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
-
-  return thisIsCrazy();
+  thisIsCrazy();
 }
 
 function sayMyName() {
   // fix the code in here:
-  var name = "Cricky";
+  var name;
 
 
 
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
   }
   sayMy();
 }
